3769011eaf1da2c2dd590450a475797cf91ab7cd
Tal Wind <talwind@Tals-MacBook-Pro.local>
2025-03-06 07:54:08 +0200

    page

--- client/src/components/MoviePage.js
+++ client/src/components/MoviePage.js
@@ -1,21 +1,34 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { useNavigate, useParams } from "react-router-dom";
-
+import "./MoviePage.css"; 
 
 
 function MoviePage() {
-  const  id  = useParams();
-useEffect(() => {
-console.log("Movie ID from URL:", id);
-})
+  const  {id}  = useParams();
+  const [movie, setMovie] = useState(null); 
+  useEffect(() => {
+    console.log("Fetching movie details...");
+    fetch(`http://localhost:3000/movies/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        
+        setMovie(data[0]); 
+      })
+      .catch((error) => console.error("Error fetching movie:", error));
+  }, [id]);
 
   const navigate = useNavigate(); 
-  return
-
-  <div>  <p>ggggg</p>
-  <button onClick={() => navigate(`/`)}>Home</button>
-  <h1>MoviePage {id}</h1>;
-  </div>
+  return movie!=null? (<div className="movie-container"> 
+  <button onClick={() => navigate(`/`)}>back</button>
+  <h3> {movie['title']} ({movie['released']})</h3>
+  <img src={movie['image']} alt={movie['title']}/>
+  {/* <h5>Released: </h5> */}
+  <h5> 📖 {movie['synopsis']}</h5>
+  <h5>Rating: {movie['rating']}</h5>
+  <h5>Duration: {movie['runtime']}</h5>
+  
+  </div>):<p>loading</p>
 
 }
 
